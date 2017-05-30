e35dd366d138518523ceafa99ddb0be704302d3c
hoye3 <sgames12@gmail.com>
2017-05-30 09:31:59 -0700

    Android studio not working
    Copied code to google doc and added location manager there. Tried to
    reinstall android studio several times, SDK still not working. Created
    several new projects and deleted them because SDK wasn't working.

--- MapApp/app/src/main/java/com/example/clifners6171/mapapp/MapsActivity.java
+++ MapApp/app/src/main/java/com/example/clifners6171/mapapp/MapsActivity.java
@@ -1,12 +1,8 @@
 package com.example.clifners6171.mapapp;
 
-import android.content.Context;
-import android.content.pm.PackageManager;
-import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
-import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -26,7 +22,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-
     }
 
 
@@ -43,33 +38,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in place born and move the camera
-        LatLng born = new LatLng(32.7157, -117.1611);
-        mMap.addMarker(new MarkerOptions().position(born).title("Born here"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(born));
-
-        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
-        }
-        mMap.setMyLocationEnabled(true);
-
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
-
-    public void mapView() {
-        if(mMap.getMapType() == GoogleMap.MAP_TYPE_SATELLITE)
-            mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
-        else
-            mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
-
-    }
-
-
-
 }
